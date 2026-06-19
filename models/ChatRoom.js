9d4949716880e5e90323b893686517133f719dc8
wanchai7 <wanchai.view47@gmail.com>
2026-06-19 15:25:26 +0700

    update model ChatRoom

--- models/ChatRoom.js
+++ models/ChatRoom.js
@@ -1,35 +1,49 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../config/db');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../config/db");
 
-const ChatRoom = sequelize.define('ChatRoom', {
+const ChatRoom = sequelize.define(
+  "ChatRoom",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     adminId: {
-        type: DataTypes.INTEGER,
-        allowNull: true
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
     status: {
-        type: DataTypes.ENUM('open', 'closed'),
-        defaultValue: 'open'
+      type: DataTypes.ENUM("open", "closed"),
+      defaultValue: "open",
     },
     lastMessage: {
-        type: DataTypes.TEXT,
-        allowNull: true
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
     lastMessageAt: {
-        type: DataTypes.DATE,
-        allowNull: true
-    }
-}, {
-    tableName: 'chat_rooms',
-    timestamps: true
-});
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    topic: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "ไม่มีหัวข้อ",
+    },
+    detail: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+      defaultValue: "",
+    },
+  },
+  {
+    tableName: "chat_rooms",
+    timestamps: true,
+  },
+);
 
 module.exports = ChatRoom;
